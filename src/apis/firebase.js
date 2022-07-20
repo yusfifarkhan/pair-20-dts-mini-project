a0842d714dd4102220c5e2286e0ec37503fff675
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-07-20 15:38:18 +0700

    feat: add env variable

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -10,12 +10,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzCet0JGW0vPUSxvsk6JzjJA4B9_jrwHA",
-  authDomain: "dts-miniproject-pair20.firebaseapp.com",
-  projectId: "dts-miniproject-pair20",
-  storageBucket: "dts-miniproject-pair20.appspot.com",
-  messagingSenderId: "234051586286",
-  appId: "1:234051586286:web:df3dc4bab22024e2e8cbff",
+  apiKey: "process.env.REACT_APP_API_KEY",
+  authDomain: "process.env.REACT_APP_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_MESSAGING_SENDER_ID",
+  appId: "process.env.REACT_APP_APP_ID",
 };
 
 // Initialize Firebase
--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -5,7 +5,7 @@ const tmdbInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
     // TODO: Jangan lupa masukkan API_KEY yang benarnya di sini yah !
-    api_key: "1415dc66b6ef1d7172388a426b3b7a65",
+    api_key: "process.env.REACT_APP_TMDB_KEY.",
   },
 });
 
